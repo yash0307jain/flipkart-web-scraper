50d87eb4ae8fd31e2acf7dc391710739cfc13501
Yash Jain <yash0307jain@gmail.com>
2020-06-29 11:09:16 +0530

    extracting the specification of all categories and rating of product as well

--- flipkart_scraper/spiders/flipkart_scraper.py
+++ flipkart_scraper/spiders/flipkart_scraper.py
@@ -3,7 +3,7 @@ import scrapy
 class FlipkartScraper(scrapy.Spider):
   name = "flipkart_scraper"
   headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.2840.71 Safari/539.36'}
-
+  rating = []
   def start_requests(self):
     urls = [
       "https://www.flipkart.com/search?q=gaming+laptop&as=on&as-show=on&otracker=AS_Query_HistoryAutoSuggest_1_7_na_na_na&otracker1=AS_Query_HistoryAutoSuggest_1_7_na_na_na&as-pos=1&as-type=HISTORY&suggestionId=gaming+laptop&requestId=c1937f69-4b94-4090-952f-47478fe51af1&as-searchtext=gaming+&sort=price_asc"
@@ -24,5 +24,29 @@ class FlipkartScraper(scrapy.Spider):
     laptopName = response.xpath("//span[@class='_35KyD6']//text()").get()
     laptopPrice = response.xpath("//div[@class='_1vC4OE _3qQ9m1']//text()").get()
     laptopHighlights = response.xpath("//div[@class='g2dDAR']/div[@class='_3WHvuP']/ul/li[@class='_2-riNZ']//text()").getall()
-    laptopSpec = response.xpath("//div[@class='MocXoX']/div/div[@class='_3Rrcbo V39ti-']/div[@class='_2RngUh']").getall()
+    laptopSpecifications = []
 
+    laptopSpec = response.xpath("//div[@class='MocXoX']/div/div[@class='_3Rrcbo V39ti-']/div[@class='_2RngUh']")
+    for spec in laptopSpec:
+      tempSpec = []
+      specTitle = spec.xpath("div[@class='_2lzn0o']/text()").get()
+      tempSpec.append(specTitle)
+
+      deepSpec = []
+      specItems = spec.xpath("table[@class='_3ENrHu']/tbody/tr[@class='_3_6Uyw row']")
+      for item in specItems:
+        tempDeepSpec = []
+        specLabel = item.xpath("td[@class='_3-wDH3 col col-3-12']//text()").get()
+        specValue = item.xpath("td[@class='_2k4JXJ col col-9-12']//text()").get()
+
+        tempDeepSpec.append(specLabel)
+        tempDeepSpec.append(specValue)
+
+        deepSpec.append(tempDeepSpec)
+
+      tempSpec.append(deepSpec)
+
+      laptopSpecifications.append(tempSpec)
+    
+    laptopRating = response.xpath("//div[@class='col-12-12 _11EBw0']/div[@class='_1i0wk8']//text()").get()
+    
